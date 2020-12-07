672e39c27c12db08438f3048495b2522d31da3fd
Cole <cbagshaw@uwo.ca>
2020-12-07 11:48:18 -0500

    connected with mySql

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'flipflopper2000'
+})
 app.use(express.static(path.join(__dirname, 'directory here')));
 
 app.get('/', (req, res) => {
