9d0a2192cff5f2cf57572a5683d9a8652559f8bf
Cole <cbagshaw@uwo.ca>
2020-12-07 12:01:10 -0500

    node.js and mysql linked

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'flipflopper2000'
-})
-app.use(express.static(path.join(__dirname, 'directory here')));
+  password: 'password',
+  database: 'workoutapp',
+  insecureAuth: true
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!')
+});
+
+//app.use(express.static(path.join(__dirname, 'directory here')));
 
 app.get('/', (req, res) => {
 });
